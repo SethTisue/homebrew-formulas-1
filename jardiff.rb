fa196e20a713b9d52e306332ec4d13a870ad9804
Seth Tisue <seth@tisue.net>
2023-10-31 08:35:21 -0700

    Update jardiff.rb for 1.80 release

--- jardiff.rb
+++ jardiff.rb
@@ -1,10 +1,10 @@
 class Jardiff < Formula
   desc "Diff tool for Java classes or classpaths"
   homepage "https://github.com/scala/jardiff"
-  url "https://github.com/scala/jardiff/releases/download/v1.7.0/jardiff.jar" #, using => :nounzip
-  sha256 "564793f9d64f2cf304c49d63224e2aee30a2cd1f3e7ea580b79030278ff182c9"
+  url "https://github.com/scala/jardiff/releases/download/v1.8.0/jardiff.jar" #, using => :nounzip
+  sha256 "97fbf16fc5a88457cd7eac69b684340e97859538900a998a6952cb73d6cc6a30"
 
-  depends_on "openjdk@11"
+  depends_on "openjdk@8"
 
   def install
     libexec.install "jardiff.jar"
